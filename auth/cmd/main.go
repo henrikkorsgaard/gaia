9605b1d905fa86303d756de4a010d327a085dc13
Henrik Korsgaard <korsgaard@protonmail.com>
2025-07-27 12:27:04 +0200

    chore: cleanup etc. with new config file handling

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -7,17 +7,25 @@ import (
 
 	"github.com/caarlos0/env"
 	"github.com/henrikkorsgaard/gaia/auth/server"
-
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		// TODO: Create config if not existing
+		/*
+			if errors.Is(err, fs.ErrNotExist) {
+			}*/
+		panic(fmt.Sprintf("unable to load .env file: %e", err))
+	}
+
 	config := server.Config{}
-	err := env.Parse(&config)
+	err = env.Parse(&config)
 	if err != nil {
 		panic(fmt.Sprintf("unable to parse ennvironment variables: %e", err))
 	}
-
+	//TODO:Port should come from config as well
 	log.Fatal(http.ListenAndServe(":3020", server.NewServer(config)))
 }

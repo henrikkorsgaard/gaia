3df588c5177500e42e1a7dab04a4ca9207f4fda9
Henrik Korsgaard <korsgaard@protonmail.com>
2025-07-30 22:48:47 +0200

    Feat: starting DMI integration

--- dmi/dmi/dmi.go
+++ dmi/dmi/dmi.go
@@ -3,3 +3,19 @@ package dmi
 type Config struct {
 	API_KEY string `env:"API_KEY,required"`
 }
+
+type DMIService struct {
+	API_KEY string
+}
+
+func New(cfg Config) DMIService {
+
+	return DMIService{
+		API_KEY: cfg.API_KEY,
+	}
+}
+
+func (s *DMIService) GetData() {
+	//Fetch data -> we also need models
+
+}
